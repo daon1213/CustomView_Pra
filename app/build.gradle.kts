2807fa7854347f230b395b28e050a2cadc72af19
daon1213 <diaeiyo1213@naver.com>
2023-10-25 20:01:40 +0900

    💡ViewBinding 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.daon.customview_pra"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.daon.customview_pra"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
