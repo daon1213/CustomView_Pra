ae2ca1ef44cc949d7904ef184752552581c6dc5d
daon1213 <diaeiyo1213@naver.com>
2023-10-25 19:21:23 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CustomView_Pra"
+include(":app")
+ 
